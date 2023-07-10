5e8a55e7b1d81b01376822a0b49aa5e712cf775b
zohvry <136025468+zohvry@users.noreply.github.com>
2023-07-10 20:03:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 18046829
+    API_HASH = "36e53933f738ccb259a73e80a874ee57"
+    TOKEN = "6147583930:AAGnciYvt1_mwRKCLlqMmDe-d3wFMtV2qeM"  
+    START_PIC = "https://telegra.ph/file/907fcc170b6a146a58aa7.jpg" 
+    CHAT = "-1001734030256"    
